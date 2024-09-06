9e4ed3b80c4fece4c63ded65aa68e53d2e3b1341
sabil <sabil.danish1997@gmail.com>
2024-09-06 22:37:12 +0530

    deployment testing

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
+require('dotenv').config()
 
 const app = express()
 
+const PORT = process.env.PORT || 4001
 
 app.get('/',async(req,res) => {
     res.json({message: "Hello World"})
 })
 
 
-app.listen(4001,console.log("connected"))
+app.listen(PORT,console.log(`connected to port number ${PORT}`))
